30d13d981510f95320fe35d7c72d68f19d04eedd
teamo <tgodliug@qq.com>
2023-07-30 18:58:58 +0800

    first commit

---
+++ demo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+struct Time_Count
+{
+    std::chrono::time_point<std::chrono::steady_clock> start, end;
+    std::chrono::duration<float> duration;
+
+    Time_Count()
+    {   
+        start = std::chrono::steady_clock::now();
+    };
+
+    ~Time_Count()
+    {
+        end = std::chrono::steady_clock::now();
+        duration = end - start;
+
+        float ms = duration.count() * 1000.0f;
+        std::cout << "time cost:" << ms << "ms" << std::endl;
+    };
+};
+
